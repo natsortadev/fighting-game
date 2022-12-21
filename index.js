88aea82500b1ce260953505019e5b20bee5794c0
nat thunder <62911376+nat-thunder@users.noreply.github.com>
2022-12-20 22:24:10 -0300

    making comments more consistent

--- index.js
+++ index.js
@@ -10,44 +10,45 @@ const gravity = .7
 
 // POO
 class Sprite {
-    // a function that is fired every time you create a new object
-    // adding the {} makes it all one object, and now it doesn't matter the order of parameters, since
-    // they are now named parameters
     constructor({position, velocity}) {
+        // a function that is fired every time you create a new object
+        // adding the {} makes it all one object, and now it doesn't matter the order of parameters, since
+        // they are now named parameters
         this.position = position;
         // because each sprite might have a different position, we receive its position as a parameter
         // and assign it to the this.position variable 
         this.velocity = velocity
-        // so the characters can fall down and be affected by gravity
+        // so the characters can move
         this.height = 150
+        this.lastKey
         // tracks which was the last key pressed so, even if both are being pressed, the character moves
         // accordingly to the last key pressed
-        this.lastKey
     }
 
-    // give the sprite an appearance
+    // gives the sprite an appearance
     draw() {
         c.fillStyle = 'red'
         // x position, y position, width and height
         c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
-    // updates the sprite whenever it moves
+    
     update() {
+        // updates the sprite whenever it moves
         this.draw()
         this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
         // its vertical position receives this.velocity.y plus itself each time update() is called
         // but zero plus zero is still zero
-        this.position.y += this.velocity.y
-        // this.position.y marks the top of the character
-        // so if we add this.height, it's the bottom
-        // when the character hits the bottom, we want it to stop
         if(this.position.y + this.height +this.velocity.y >= canvas.height) {
+            // this.position.y marks the top of the character
+            // so if we add this.height, it's the bottom
+            // when the character hits the bottom, we want it to stop
             this.velocity.y = 0
         } else this.velocity.y += gravity
         // gravity to close the gap between the character and the ground to make sure that,
         // as long as there is space between the character and bottom of canvas, it 
-        // continues following
+        // continues falling
     }
 }
 
@@ -91,13 +92,13 @@ const keys = {
     },
 }
 
-// give the sprites animations
+// gives the sprites animations
 // a loop
 // go crazy go dumb
 function animate() {
     window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
     // 'black' to keep the background black
+    c.fillStyle = 'black'
     // this "fill" sort of erases the previous draw() so the character looks like it's actually 
     // falling, not extending forever
     c.fillRect(0, 0, canvas.width, canvas.height)
