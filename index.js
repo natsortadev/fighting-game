9e46a1b4f2e9841887e31067cf834995ba5677a8
nat thunder <62911376+nat-thunder@users.noreply.github.com>
2022-12-26 22:07:07 -0300

    26-12-2022

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const gravity = .7
 
 // POO
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({ position, velocity, color = 'red', offset}) {
         // a function that is fired every time you create a new object
         // adding the {} makes it all one object, and now it doesn't matter the order of parameters, since
         // they are now named parameters
@@ -19,28 +19,51 @@ class Sprite {
         // and assign it to the this.position variable 
         this.velocity = velocity
         // so the characters can move
+        this.width = 50
         this.height = 150
         this.lastKey
         // tracks which was the last key pressed so, even if both are being pressed, the character moves
         // accordingly to the last key pressed
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            }, // follows the character's position
+            offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+        this.isAttacking
     }
 
     // gives the sprite an appearance
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = this.color
         // x position, y position, width and height
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // attack box
+        if (this.isAttacking) {
+            c.fillStyle = 'green'
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        }
     }
 
     
     update() {
         // updates the sprite whenever it moves
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        // this whole bamboozle changes the direction of the enemy's attackbox
+        this.attackBox.position.y = this.position.y
+        // updates the attack box's position to match the character's
+
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         // its vertical position receives this.velocity.y plus itself each time update() is called
         // but zero plus zero is still zero
-        if(this.position.y + this.height +this.velocity.y >= canvas.height) {
+        if (this.position.y + this.height +this.velocity.y >= canvas.height) {
             // this.position.y marks the top of the character
             // so if we add this.height, it's the bottom
             // when the character hits the bottom, we want it to stop
@@ -49,6 +72,15 @@ class Sprite {
         // gravity to close the gap between the character and the ground to make sure that,
         // as long as there is space between the character and bottom of canvas, it 
         // continues falling
+    }
+
+    attack() {
+        // keeps track of whether the player is attacking or not
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
+        // sets isAttacking back to false after a while so the player isn't stuck attacking forever
     }
 }
 
@@ -62,6 +94,10 @@ const player = new Sprite({
         y: 10,
         // the sprite is not moving by default
     },
+    offset: {
+        x: 0,
+        y: 0,
+    },
 })
 
 const enemy = new Sprite({
@@ -71,6 +107,11 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
+        y: 0,
+    },
+    color: 'blue',
+    offset: {
+        x: -50,
         y: 0,
     },
 });
@@ -92,10 +133,26 @@ const keys = {
     },
 }
 
-// gives the sprites animations
-// a loop
-// go crazy go dumb
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+        // rectangle1.attackBox.position.x + rectangle1.attackBox.width to get the right
+        // side of the attack box, once the position uses the left side as reference
+        // if the position the right side of the attack box passes through the 
+        // position of the left side of the rectangle2 (a.k.a. is greater or equal to),
+        // that means they're touching
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        // if the left side of the attack box is no longer touching the rectangle2
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+        // so we know if rectangle1 is actually touching rectangle2 and not just floating above it
+    )
+}
+
 function animate() {
+    // gives the sprites animations
+    // a loop
+    // go crazy go dumb
     window.requestAnimationFrame(animate)
     // 'black' to keep the background black
     c.fillStyle = 'black'
@@ -119,12 +176,37 @@ function animate() {
     }
 
     // player 2
-    if(keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
         // moves to the left
         enemy.velocity.x = -5
-    } else if(keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         // moves to the right
         enemy.velocity.x = 5
+    }
+
+    // detect for collision
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy,
+        }) &&
+        player.isAttacking
+    ) {
+        player.isAttacking = false 
+        // so the enemy doesn't get hit multiple times during timeout
+        console.log('player attack')
+    }
+
+    if (
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player,
+        }) &&
+        enemy.isAttacking
+    ) {
+        enemy.isAttacking = false 
+        // so the enemy doesn't get hit multiple times during timeout
+        console.log('enemy attack')
     }
 }
 
@@ -134,7 +216,7 @@ animate()
 window.addEventListener('keydown', (event) => {
     console.log(event.key)
     // event.key => shows which key the player pressed
-    switch(event.key) {
+    switch (event.key) {
         // player 1
         case 'd':
             keys.d.pressed = true
@@ -147,8 +229,10 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -20
             break
-    }
-    switch(event.key) {
+        case ' ':
+            player.attack()
+            break
+
         // player 2
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
@@ -161,13 +245,15 @@ window.addEventListener('keydown', (event) => {
         case 'ArrowUp':
             enemy.velocity.y = -20
             break
+        case 'ArrowDown':
+            enemy.attack()
+            break
     }
-    console.log(event.key)
 })
 
 // keyup => when the player lets go of the key
 window.addEventListener('keyup', (event) => {
-    switch(event.key) {
+    switch (event.key) {
         // player 1
         case 'd':
             keys.d.pressed = false
@@ -175,8 +261,7 @@ window.addEventListener('keyup', (event) => {
         case 'a':
             keys.a.pressed = false
             break
-    }
-    switch(event.key) {
+
         // player 2
         case 'ArrowRight':
             keys.ArrowRight.pressed = false
@@ -185,5 +270,4 @@ window.addEventListener('keyup', (event) => {
             keys.ArrowLeft.pressed = false
             break
     }
-    console.log(event.key)
 })
--- main.md
+++ main.md
@@ -2,6 +2,6 @@
 - *Project Setup (done / 20-12-2022)*
 - *Create Player and Enemy (done / 20-12-2022)*
 - *Move Characters with Event Listeners (done / 20-12-2022)*
-- Attacks
+- *Attacks (done / 26-12-2022)*
 - Health Bar Interface
 - Game Timers and Game Over
